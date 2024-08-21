754310c4ffae22f826e443a5dcdc7860fff76e7d
FATEC | João Victor Soares Dias <fatec.jdias@icolabora.com.br>
2024-08-21 15:21:21 -0300

    logando e gerando token JWT

--- backend/src/services/users/AuthUserService.ts
+++ backend/src/services/users/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
     email: string;
@@ -26,7 +27,26 @@ class AuthUserService {
             throw new Error("User/password incorrect!");
         }
 
-        
+        // Se deu tudo certo -> Gerar um token JWT e devolver os dados do usuario
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        );
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
+
     }
 }
 
