19aa97cfedab256e23dfbbd15e2f17b6a9be48b9
FATEC | João Victor Soares Dias <fatec.jdias@icolabora.com.br>
2024-08-21 13:36:33 -0300

    cadastrando user

--- backend/src/services/users/createUserService.ts
+++ backend/src/services/users/createUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma"; 
+
 interface UserRequest{
     name: string;
     email: string;
@@ -6,7 +8,36 @@ interface UserRequest{
 
 class CreateUserService {
     async execute({name, email, password} : UserRequest){
+        // Verificar se ele enviou um e-mail
+        if(!email) {
+            throw new Error("E-mail incorrect!");
+        }
 
+        // Verificar se o e-mail já está cadastrado na plataforma
+        const userAlredyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        });
+
+        if(userAlredyExists) {
+            throw new Error("User alredy exists!");
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        });
+
+        return user;
     }
 }
 
