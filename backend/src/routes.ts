83e9def3ba9e5362334fd7727c998be46eebbfdd
FATEC | João Victor Soares Dias <fatec.jdias@icolabora.com.br>
2024-08-29 00:32:51 -0300

    rota products

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,19 +9,20 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 const router = Router();
 
-// ROTAS USER 
+// --- ROTAS USER ---
 router.post('/users', new CreateUserController().handle);
-
 router.post('/session', new AuthUserController().handle);
-
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 
-// ROTAS CATEGORY
+// --- ROTAS CATEGORY ---
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
-
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+// --- ROTAS PRODUCT ---
+router.post('/product', isAuthenticated, new CreateProductController().handle);
 
 export { router }
