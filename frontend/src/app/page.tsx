d129119c49d4dbaa546782eae0eed913289b3fb3
FATEC | João Victor Soares Dias <fatec.jdias@icolabora.com.br>
2024-09-18 16:31:34 -0300

    criando pagina de login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,40 @@
 import styles from "./page.module.scss";
+import logoImg from "/public/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main>
-      <h1 className={styles.teste}>Teste</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo da pizzaria" />
+
+        <section className={styles.login}>
+          <form action="">
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu e-mail..."
+              className={styles.input}
+            />
+
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="*****"
+              className={styles.input}
+            />
+
+            <button type="submit">Acessar</button>
+          </form>
+
+          <Link href="/signUp" className={styles.text}>
+            Não Possui uma conta? Cadastre-se
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
